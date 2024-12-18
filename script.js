d704aac05c89dd29529c5311e8562bda0c44cf84
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2024-12-18 13:48:02 -0300

    jsw

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Estoy vinculado pa")
+// Variables
+const url = 'api';
+const contenedor = document.querySelector('tbody');
+let resultado = ''
+const modalArticulos = new bootstrap.Modal(document.getElementById('modalArticulo'));
+const formArticulo = document.querySelector('form');
+const descripcion = document.getElementById('descripcion');
+const precio = document.getElementById('precio');
+const stock = document.getElementById('stock');
+let opcion = ''
+
+btnCrear.addEventListener('click', () => {
+    modalArticulos.show();
+});
+
